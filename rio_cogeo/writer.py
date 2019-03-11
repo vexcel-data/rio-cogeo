09c310484cd96fdd8c3360c0e789c5ab5365ff0a
Holmes555 <Ivan.bazhenov333@gmail.com>
2019-03-11 17:06:52 +0300

    Use separate credentials for AWS.

--- rio_cogeo/writer.py
+++ rio_cogeo/writer.py
@@ -19,12 +19,23 @@ def write_to_dst(mem, memfile, dst_path, dst_kwargs):
             file_key = '/'.join(dst_path.split('/')[v + 1:])
             bucket_name = dst_path.split('/')[v]
 
+            AWS_ACCESS_KEY_ID: 'str' = os.environ.get(
+                'AWS_ACCESS_KEY_ID_OUT',
+                getenv()['AWS_ACCESS_KEY_ID']
+            )
+            AWS_SECRET_ACCESS_KEY: 'str' = os.environ.get(
+                'AWS_SECRET_ACCESS_KEY_OUT',
+                getenv()['AWS_SECRET_ACCESS_KEY']
+            )
+            AWS_SESSION_TOKEN: 'str' = os.environ.get('AWS_SESSION_TOKEN_OUT')
+            AWS_REGION_NAME: 'str' = os.environ.get('AWS_DEFAULT_REGION_OUT')
+            AWS_PROFILE_NAME: 'str' = os.environ.get('AWS_DEFAULT_PROFILE_OUT')
             session = boto3.Session(
-                aws_access_key_id=getenv()['AWS_ACCESS_KEY_ID'],
-                aws_secret_access_key=getenv()['AWS_SECRET_ACCESS_KEY'],
-                aws_session_token=getenv().get('AWS_SESSION_TOKEN'),
-                region_name=getenv().get('AWS_REGION_NAME'),
-                profile_name=getenv().get('AWS_PROFILE_NAME')
+                aws_access_key_id=AWS_ACCESS_KEY_ID,
+                aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                aws_session_token=AWS_SESSION_TOKEN,
+                region_name=AWS_REGION_NAME,
+                profile_name=AWS_PROFILE_NAME
             )
             s3 = session.client('s3')
             s3.upload_fileobj(memfile, bucket_name, file_key)
